80dfb6bf7dc775d0a58a10d3483135da530d6e83
June <df41022@gmail.com>
2024-11-16 10:34:19 +0700

    dummy generate key pair

---
+++ packages/xmtp-bot/maci/packages/contracts/ts/index.ts
@@ -0,0 +1,42 @@
+export {
+  deployMockVerifier,
+  deployVkRegistry,
+  deployMaci,
+  deployContract,
+  deployContractWithLinkedLibraries,
+  deploySignupToken,
+  deploySignupTokenGatekeeper,
+  deployConstantInitialVoiceCreditProxy,
+  deployFreeForAllSignUpGatekeeper,
+  deployGitcoinPassportGatekeeper,
+  deploySemaphoreGatekeeper,
+  deployPollFactory,
+  createContractFactory,
+  deployPoseidonContracts,
+  deployVerifier,
+} from "./deploy";
+export { genMaciStateFromContract } from "./genMaciState";
+export { genEmptyBallotRoots } from "./genEmptyBallotRoots";
+export { formatProofForVerifierContract, getDefaultSigner, getDefaultNetwork, getSigners } from "./utils";
+export { EMode } from "./constants";
+export { EDeploySteps } from "../tasks/helpers/constants";
+export { Deployment } from "../tasks/helpers/Deployment";
+export { ContractStorage } from "../tasks/helpers/ContractStorage";
+export { ProofGenerator } from "../tasks/helpers/ProofGenerator";
+export { TreeMerger } from "../tasks/helpers/TreeMerger";
+export { Prover } from "../tasks/helpers/Prover";
+export {
+  EContracts,
+  EGatekeepers,
+  EInitialVoiceCreditProxies,
+  type IGenerateProofsOptions,
+  type IGenerateProofsBatchData,
+  type TallyData,
+  type IDeployParams,
+  type IMergeParams,
+  type IProveParams,
+} from "../tasks/helpers/types";
+export { linkPoseidonLibraries } from "../tasks/helpers/abi";
+
+export type { IVerifyingKeyStruct, SnarkProof, Groth16Proof, Proof } from "./types";
+export * from "../typechain-types";
---
+++ packages/xmtp-bot/maci/src/routes/getKeyPair.js
@@ -0,0 +1,7 @@
+export default function getKeyPair() {
+    // This is just an example of generating a random key pair
+    return {
+        publicKey: 'public-key-example',
+        privateKey: 'private-key-example',
+    };
+}
